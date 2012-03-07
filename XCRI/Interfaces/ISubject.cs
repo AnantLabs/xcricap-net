a9a00a51fa3b44dde95e6fb5ac325b78e1aeac0d
Jade Widdowson <Jade Widdowson@aa3647-PC.coventry.ac.uk>
2012-03-07 09:29:53 +0000

    Changed to implement XCRICAP12.ISubject

--- XCRI/Interfaces/ISubject.cs
+++ XCRI/Interfaces/ISubject.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace XCRI.Interfaces
 {
-    public interface ISubject : XCRICAP11.ISubject, XCRICAP12.ITitle
+    public interface ISubject : XCRICAP11.ISubject, XCRICAP12.ISubject
     {
     }
 }
